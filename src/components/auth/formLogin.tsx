b6017ba3567c85ffc1df5db2dd22cf4e161e295f
Aadarsha Subedi <subediaadarsha0@gmail.com>
2025-06-23 19:12:09 +0545

    Fixed login form

--- src/components/auth/formLogin.tsx
+++ src/components/auth/formLogin.tsx
@@ -175,12 +175,6 @@ const LoginForm = () => {
                   Remember me
                 </label>
               </div>
-              <Link
-                href="/forgot-password"
-                className="text-sm text-blue-600 hover:text-blue-700 font-medium"
-              >
-                Forgot password?
-              </Link>
             </div>
 
             {/* Submit Button */}
